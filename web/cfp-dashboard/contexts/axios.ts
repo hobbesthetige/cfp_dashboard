86dd3a5b016ddc27f4e64160df720343b52b2ba8
Daniel Meachum <daniel.meachum@gmail.com>
2024-08-29 20:44:59 -0400

    Commit changes to env variables

--- web/cfp-dashboard/contexts/axios.ts
+++ web/cfp-dashboard/contexts/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseURL = `${process.env.NEXT_PUBLIC_SERVER_URL}/api`;
-export const ansibleURL = 'http://ansible:5001/';
+export const ansibleURL = process.env.NEXT_PUBLIC_ANSIBLE_URL;
 export const axiosInstance = axios.create({
   baseURL: baseURL,
 });
